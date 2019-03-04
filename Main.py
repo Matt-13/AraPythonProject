c7b8a6d44431f572f7ace2978035f2477aeb2692
Matt_13 <matthew130713@gmail.com>
2019-03-04 16:01:45 +1300

    Added liam's code

--- Main.py
+++ Main.py
@@ -1,1 +1,24 @@
 print("Hello World")
+
+
+class MakeClassDiagram:
+    def __init__(self, filename):
+        self.allMyClasses = []
+        self.code = filename
+
+    def count_occurrences(self, word, sentence):
+        return sentence.lower().split().count(word)
+
+    def find_classes(self):
+        value = self.count_occurrences("class", self.code)
+
+        for i in range (0, value):
+            self.allMyClasses.append(self.code.split("class")[i + 1])
+
+
+filename = "class MakeClassDiagram: def __init__(self, filename): self.allMyClasses = [] self.code = " \
+           "filename def count_occurrences(self, word, sentence): return sentence.lower().split().count(word)" \
+           "class Test: def __init__()"
+mkd = MakeClassDiagram(filename)
+mkd.find_classes()
+print(mkd.allMyClasses)
