40336cfe1320dd309877cb6eb36399645f37c94a
Matthew Whitaker <matthew130713@gmail.com>
2019-03-09 09:47:39 +1300

    Added PlantUML jarfile, updated python packages.

--- venv/Scripts/pip-script.py
+++ venv/Scripts/pip-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\mgw0087\PycharmProjects\AraPythonProject\venv\Scripts\python.exe
+#!C:\Users\Matthew\PycharmProjects\AraPythonProject\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==10.0.1','console_scripts','pip'
 __requires__ = 'pip==10.0.1'
 import re
