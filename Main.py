d8301f59ccb850a12a849e335f2a19b9836b6043
Matthew Whitaker <matthew130713@gmail.com>
2019-03-14 20:46:08 +1300

    Added checks at the start to see if the file begins with @startuml and ends with @enduml.
    Also Added Sarah's code for inspiration, will refactor and get working with my classes.

--- Main.py
+++ Main.py
@@ -12,23 +12,40 @@ class ReadPlantUML:
         self.allMyClasses = []
         self.code = filename
 
+    def check_if_plantuml(self, code):
+        try:
+            if code.split(' ', 1)[0] == "@startuml" and code.endswith("@enduml"):
+                return True
+            else:
+                return False
+        except Exception as e:
+            print(e)
+
     # Check if the file contains the word "Class"
     def count_occurrences(self, word, sentence):
         try:
             if sentence.lower().split().count(word) > 0:
                 return sentence.lower().split().count(word)
             elif sentence.lower().split().count(word) == 0:
-                print("Word not found.")
+                print("Classes not found.")
         except Exception as e:
             print(e)
 
     def find_classes(self):
-        value = self.count_occurrences("class", self.code)
+        try:
+            isplantuml = self.check_if_plantuml(self.code)
+            if isplantuml:
+                print("File Accepted! Continuing..")
+                value = self.count_occurrences("class", self.code)
 
-        for i in range(0, value):
-            self.allMyClasses.append(self.code.split("class")[i + 1])
+                for i in range(0, value):
+                    self.allMyClasses.append(self.code.split("class")[i + 1])
 
-        return self.allMyClasses
+                return self.allMyClasses
+            else:
+                print("Program Stopping..")
+        except Exception as e:
+            print(e)
 
 
 class ClassConverter:
@@ -49,12 +66,31 @@ class ClassConverter:
 class ConvertPlantUML:
     def __init__(self):
         self.classes = []
+        self.convertedclasses =[]
 
-    def check_file(self):
-        print("Checking file..")
-        # For string(class) in self.stringClasses
-        #     classobject = ClassConverter(class)
-        #     self.objectClasses.append(classobject)
+    def convert_file(self):
+        print("Converting file to python syntax..")
+        for class_info in self.classes:
+            class_name = class_info.split(' ')[1]
+            attributes = []
+            methods = []
+            for line in class_info.split("\n"):
+                if line.find(":") != -1:
+                    attributes.append(line)
+            for line in class_info.split("\n"):
+                if line.find("()") != -1:
+                    methods.append(line)
+            self.add_class(class_name, attributes, methods)
+
+    def add_class(self, class_name, attributes, methods):
+        new_class = ClassBuilder(class_name, attributes, methods)
+        new_class.add_class_attributes()
+        new_class.add_class_methods()
+        self.convertedclasses.append(new_class)
+
+    def print_program(self):
+        for x in self.convertedclasses:
+            x.print_class()
 
     def read_file(self, file):
         with open(file, "r") as filename:
@@ -63,10 +99,60 @@ class ConvertPlantUML:
         self.classes = rduml.find_classes()
 
 
+class ClassBuilder:
+    def __init__(self, class_name, new_attributes, new_methods):
+        self.name = class_name
+        self.attributes = new_attributes
+        self.methods = new_methods
+        self.all_my_attributes = []
+        self.all_my_methods = []
+
+    def add_class_attributes(self):
+        for an_attribute in self.attributes:
+            new_a_name = an_attribute.split(":")[0]
+            new_a_return = an_attribute.split(":")[1]
+            new_a = Attribute(new_a_name, new_a_return)
+            self.all_my_attributes.append(new_a)
+
+    def add_class_methods(self):
+        for a_method in self.methods:
+            new_m_name = a_method.split(":")[0]
+            new_m_return = a_method.split("()")[1]
+            new_m = Method(new_m_name, new_m_return)
+            self.all_my_methods.append(new_m)
+
+    def print_class(self):
+        print("class", self.name, ":", end = "")
+        print("\n\n", "   def __init__ (self):")
+        for x in self.all_my_attributes:
+            print(x)
+        print("\n")
+        for x in self.all_my_methods:
+            print(x)
+        print("\n\n")
+
+
+class Attribute:
+    def __init__(self, new_name, new_return):
+        self.name = new_name
+        self._return = new_return
+
+    def __str__(self):
+        return f"       {self.name}= {self._return}"
+
+
+class Method:
+    def __init__(self, new_name, new_return):
+        self.name = new_name.replace("()", "")
+        self._return = new_return
+
+    def __str__(self):
+        return f"       {self.name} (self):\n               pass"
+
 rd = ConvertPlantUML()
 rd.read_file("Graph.txt")
 print(rd.classes)
-rd.check_file()
+rd.convert_file()
 
 """
 contents = Alice -> Bob: test
